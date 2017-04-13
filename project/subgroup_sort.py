def2443f102786b31ada213d83e7f0dd1487a94d
Joaquim Claivaz <Joaquim Claivaz>
2017-04-13 10:05:10 +0200

    Rewrite the function, insertion of threshold parameter

--- project/subgroup_sort.py
+++ project/subgroup_sort.py
@@ -7,72 +7,114 @@ Created on Thu Apr  6 18:28:06 2017
 Script which select different genes specific to a given subgroup of firm-5 and write its specific file
 Subgroup considered:Bombus sp. & Apis sp. & Apis/Bombus sp. &  Apis/Bombus/Out_lacto sp.
 """
-
 import os
-os.getcwd()
-os.chdir('D:\\Unil\Master\Semester 2\SAGE\project')
 
 ###set different possible species
-ortho_tab = open("Genefamilies_all.txt",'r')
-ortho_tab.readline()
+Ortho_tab = open("data/Genefamilies_all.txt",'r')
+Ortho_tab.readline()
 
 species_list=[]
-for line in ortho_tab:
-    species=line.split("\t")
-    for spec in species:
-        tmp_spec=spec.split("|")[0]
-        if tmp_spec not in species_list and tmp_spec.split("_")[0]!="Gene":
-            species_list.append(tmp_spec)
+for line in Ortho_tab:
+    split_line_bytab=line.split("\t")
+    for obj in split_line_bytab:
+        species=obj.split("|")[0]
+        if species not in species_list and species.split("_")[0]!="Gene":
+            species_list.append(species)
 
-set(species_list)
+print(set(species_list))
 
 #####create different subgroup
 #'JF72':Lactobacillus apis ,'JF73':Lactobacillus helsingborgensis,'JF74':Lactobacillus melliventris,'JF75':Lactobacillus kimbladii,'JF76':Lactobacillus kullabergensis,'JG29':Lactobacillus mellis,'JG30':Lactobacillus mellifer,'L183':L. helsingborgensis,'L184':L melliventris, 'L185': L apis, 'L186': L Kullabergensis, 'LA14': Lactobacillus acidophilus (mammalian commensale/yoghurt), 'LA2':Lactobacillus amylovorus (pig) , 'LDB': L. delbrueckii (yoghurt), 'LGAS':Lactobacillus gasseri (Prebiotic/probiotic), 'LHV': Lactobacillus helveticus (cheese process),'LJP': Lactobacillus johnsonii (probiot/prebiot), 'WANG': L kefiranofaciens (pro/prebiot/kefir)
-B_pascuorum=['F225','F230', 'F233', 'F234','F236','F237']
+B_pascuorum=['F225','F230','F233','F234','F236','F237']
 B_bohemicus=['F228', 'F245','F246','F247']
-A_melifera=['F259','F260','F261','F262','F263']
-Out_lacto=['JF72','JF73','JF74','JF75','JF76','JG29','JG30','L183','L184', 'L185', 'L186']
-Outgroup=['LA14', 'LA2', 'LDB', 'LGAS', 'LHV','LJP', 'WANG']
+Bumble=B_pascuorum+B_bohemicus
+A_mellifera=['JF72','JF73','JG30','JF74','JF75','JF76','F259','F260','F261','F262','F263','L183','L184','L185','L186']
+Honey=A_mellifera
+Outgroup_wo_yogurt=['JG29']
+Outgroup_w_yogurt=['LA14','LA2','LDB','LGAS','LHV','LJP','WANG']
 #####loop writing the correct set/subgroup
-ortho_tab = open("Genefamilies_all.txt",'r')
-set1=open("set1.txt",'w') #Bumble set
-set2=open("set2.txt","w") #Apis set
-set3=open("set3.txt","w") #Bumble and Apis set
-set4=open("set4.txt","w") #Bumble,Apis and Out_lacto set
+Ortho_tab=open("data/Genefamilies_all.txt",'r')
+Bumble_bees_f=open("Bumble_bees.txt",'w') #Bumble set
+Honey_bees_f=open("Honey_bees.txt","w") #Apis set
+Bumble_Honey_bees_f=open("Bumble_Honey_bees.txt","w") #Bumble and Apis set
+Bumble_Honey_Lacto_bees_f=open("Bumble_Honey_Lacto_bees.txt","w") #Bumble,Apis and Out_lacto set
+#
+#for line in Ortho_tab:
+#    B_bohemicus_TF=False
+#    B_pascuorum_TF=False
+#    A_melifera_TF=False
+#    Outgroup_wo_yogurt_TF=False
+#    Outgroup_w_yogurtTF=False
+#    split_line_bytab=line.split("\t")
+#    for obj in split_line_bytab:
+#        strains=obj.split("|")[0]
+#        if strains in Outgroup:
+#            Outgroup_TF=True
+#            break
+#        elif strains in B_pascuorum:
+#            B_pascuorum_TF=True
+#        elif strains in B_bohemicus:
+#            B_bohemicus_TF=True
+#        elif strains in A_melifera:
+#            A_melifera_TF=True
+#        elif strains in Out_lacto:
+#            Out_lacto_TF=True
+#    if not Outgroup_TF:
+#        if B_bohemicus_TF and B_pascuorum_TF:
+#            if not A_melifera_TF and not Out_lacto_TF:
+#                Bumble_bees_f.write(line)
+#            elif A_melifera_TF and not Out_lacto_TF:
+#                Bumble_Honey_bees_f.write(line)
+#            else:
+#                Bumble_Honey_Lacto_bees_f.write(line)
+#        elif A_melifera_TF and not Out_lacto_TF and not B_bohemicus_TF and not B_pascuorum_TF:
+#            Honey_bees_f.write(line)
+#            
+#Bumble_bees_f.close()    
+#Honey_bees_f.close()
+#Bumble_Honey_bees_f.close()
+#Bumble_Honey_Lacto_bees_f.close()
+#Ortho_tab.close()
 
-for line in ortho_tab:
-    b_b=False
-    b_p=False
-    a_m=False
-    o_l=False
-    o_g=False
-    tmp=line.split("\t")
-    for tmp_seq in tmp:
-        seqtmp=tmp_seq.split("|")[0]
-        if seqtmp in Outgroup:
-            o_g=True
-            break
-        elif seqtmp in B_pascuorum:
-            b_p=True
-        elif seqtmp in B_bohemicus:
-            b_b=True
-        elif seqtmp in A_melifera:
-            a_m=True
-        elif seqtmp in Out_lacto:
-            o_l=True
-    if not o_g:
-        if b_b and b_p:
-            if not a_m and not o_l:
-                set1.write(line)
-            elif a_m and not o_l:
-                set3.write(line)
-            else:
-                set4.write(line)
-        elif a_m and not o_l and not b_b and not b_p:
-            set2.write(line)
-            
-set1.close()    
-set2.close()
-set3.close()
-set4.close()
-ortho_tab.close()
+def are_all_in_line(group_bee,line,threshold):
+    threshold_limit=1
+    one_strain_present=False
+    for strain in group_bee:
+        if strain not in line:
+            threshold_limit-=1/len(group_bee)
+            if threshold_limit < threshold and one_strain_present:
+                return 'Partial'
+        else:
+            one_strain_present=True
+    if threshold_limit > threshold:
+        return True
+    elif one_strain_present:
+        return 'Partial'
+    else:
+        return False
+###define
+threshold=0
+###
+for line in Ortho_tab:
+    Bumble_tmp=are_all_in_line(Bumble,line,threshold)
+    Honey_tmp=are_all_in_line(Honey,line,threshold)
+    Outgroup_wo_yogurt_tmp=are_all_in_line(Outgroup_wo_yogurt,line,threshold)
+    Outgroup_w_yogurt_tmp=are_all_in_line(Outgroup_w_yogurt,line,threshold)
+    if not Outgroup_w_yoghurt_tmp:
+        if Bumble_tmp==True:
+            if Honey_tmp==True:
+                if Outgroup_wo_yoghurt_tmp==True:
+                    Bumble_Honey_Lacto_bees_f.write(line)
+                elif not Outgroup_wo_yogurt_tmp:
+                    Bumble_Honey_bees_f.write(line)
+            elif not Outgroup_wo_yogurt_tmp and not Honey_tmp:
+                Bumble_bees_f.write(line)
+        elif Honey_tmp==True and not Outgroup_w_yogurt_tmp and not Bumble_tmp:
+            Honey_bees_f.write(line)
+
+Bumble_bees_f.close()    
+Honey_bees_f.close()
+Bumble_Honey_bees_f.close()
+Bumble_Honey_Lacto_bees_f.close()
+Ortho_tab.close()
+       
