4c7dc7d3ef6941a9432fb68f1a59bcc1df2bfe36
Joaquim Claivaz <Joaquim Claivaz>
2017-04-14 11:56:19 +0200

    Redfinition of the different subgroup, insertion of the verification step: checking if all gene families were sorted

--- scripts/subgroup_sort.py
+++ scripts/subgroup_sort.py
@@ -10,8 +10,10 @@ Subgroup considered:Bombus sp. & Apis sp. & Apis/Bombus sp. &  Apis/Bombus/Out_l
 import os
 
 ###set different possible species
+#Ortho_tab = open("Genefamilies_all.txt",'r')
+
 Ortho_tab = open("data/Genefamilies_all.txt",'r')
-Ortho_tab.readline()
+#Ortho_tab.readline()
 
 species_list=[]
 for line in Ortho_tab:
@@ -30,14 +32,16 @@ B_bohemicus=['F228', 'F245','F246','F247']
 Bumble=B_pascuorum+B_bohemicus
 A_mellifera=['JF72','JF73','JG30','JF74','JF75','JF76','F259','F260','F261','F262','F263','L183','L184','L185','L186']
 Honey=A_mellifera
-Outgroup_wo_yogurt=['JG29']
-Outgroup_w_yogurt=['LA14','LA2','LDB','LGAS','LHV','LJP','WANG']
+Outgroup=['JG29','LA14','LA2','LDB','LGAS','LHV','LJP','WANG']
+#Outgroup_wo_yogurt=['JG29']
+#Outgroup_w_yogurt=['LA14','LA2','LDB','LGAS','LHV','LJP','WANG']
 #####loop writing the correct set/subgroup
 Ortho_tab=open("data/Genefamilies_all.txt",'r')
 Bumble_bees_f=open("Bumble_bees.txt",'w') #Bumble set
 Honey_bees_f=open("Honey_bees.txt","w") #Apis set
 Bumble_Honey_bees_f=open("Bumble_Honey_bees.txt","w") #Bumble and Apis set
-Bumble_Honey_Lacto_bees_f=open("Bumble_Honey_Lacto_bees.txt","w") #Bumble,Apis and Out_lacto set
+Outgroup_f=open("Outgroup.txt","w")
+#Bumble_Honey_Lacto_bees_f=open("Bumble_Honey_Lacto_bees.txt","w") #Bumble,Apis and Out_lacto set
 #
 #for line in Ortho_tab:
 #    B_bohemicus_TF=False
@@ -93,28 +97,83 @@ def are_all_in_line(group_bee,line,threshold):
     else:
         return False
 ###define
-threshold=0
+#threshold=0
+####
+#for line in Ortho_tab:
+#    Bumble_tmp=are_all_in_line(Bumble,line,threshold)
+#    Honey_tmp=are_all_in_line(Honey,line,threshold)
+#    Outgroup_wo_yogurt_tmp=are_all_in_line(Outgroup_wo_yogurt,line,threshold)
+#    Outgroup_w_yogurt_tmp=are_all_in_line(Outgroup_w_yogurt,line,threshold)
+#    if not Outgroup_w_yoghurt_tmp:
+#        if Bumble_tmp==True:
+#            if Honey_tmp==True:
+#                if Outgroup_wo_yoghurt_tmp==True:
+#                    Bumble_Honey_Lacto_bees_f.write(line)
+#                elif not Outgroup_wo_yogurt_tmp:
+#                    Bumble_Honey_bees_f.write(line)
+#            elif not Outgroup_wo_yogurt_tmp and not Honey_tmp:
+#                Bumble_bees_f.write(line)
+#        elif Honey_tmp==True and not Outgroup_w_yogurt_tmp and not Bumble_tmp:
+#            Honey_bees_f.write(line)
+
+threshold=0.8
 ###
 for line in Ortho_tab:
     Bumble_tmp=are_all_in_line(Bumble,line,threshold)
     Honey_tmp=are_all_in_line(Honey,line,threshold)
-    Outgroup_wo_yogurt_tmp=are_all_in_line(Outgroup_wo_yogurt,line,threshold)
-    Outgroup_w_yogurt_tmp=are_all_in_line(Outgroup_w_yogurt,line,threshold)
-    if not Outgroup_w_yoghurt_tmp:
+    Outgroup_tmp=are_all_in_line(Outgroup,line,threshold)
+    if not Outgroup_tmp:
         if Bumble_tmp==True:
             if Honey_tmp==True:
-                if Outgroup_wo_yoghurt_tmp==True:
-                    Bumble_Honey_Lacto_bees_f.write(line)
-                elif not Outgroup_wo_yogurt_tmp:
-                    Bumble_Honey_bees_f.write(line)
-            elif not Outgroup_wo_yogurt_tmp and not Honey_tmp:
+                Bumble_Honey_bees_f.write(line)
+            elif not Honey_tmp:
                 Bumble_bees_f.write(line)
-        elif Honey_tmp==True and not Outgroup_w_yogurt_tmp and not Bumble_tmp:
+            else:
+                Outgroup_f.write(line)
+        elif not Bumble_tmp and Honey_tmp==True:
             Honey_bees_f.write(line)
+        else:
+            Outgroup_f.write(line)
+    else:
+        Outgroup_f.write(line)
+
+
 
 Bumble_bees_f.close()    
 Honey_bees_f.close()
 Bumble_Honey_bees_f.close()
-Bumble_Honey_Lacto_bees_f.close()
+#Bumble_Honey_Lacto_bees_f.close()
+Outgroup_f.close()
 Ortho_tab.close()
+  
+#Verification
+Ortho_tab=open("data/Genefamilies_all.txt",'r')
+Bumble_bees_f=open("Bumble_bees.txt",'r') #Bumble set
+Honey_bees_f=open("Honey_bees.txt","r") #Apis set
+Bumble_Honey_bees_f=open("Bumble_Honey_bees.txt","r") #Bumble and Apis set
+Outgroup_f=open("Outgroup.txt","r")
 
+expected_gene_family=[]
+sort_gene_family=[]
+
+def check_function(file_to_check,list_append):
+    for genefam in file_to_check:
+        if genefam.split('\t')[0].split('_')[0]=='Gene':
+            list_append.append(genefam.split('\t')[0])
+            
+check_function(Ortho_tab,expected_gene_family)
+check_function(Bumble_bees_f,sort_gene_family)
+check_function(Honey_bees_f,sort_gene_family)
+check_function(Bumble_Honey_bees_f,sort_gene_family)
+check_function(Outgroup_f,sort_gene_family)
+
+if sort_gene_family.sort()==expected_gene_family.sort():
+    print('all the gene families were sorted')
+
+
+Bumble_bees_f.close()    
+Honey_bees_f.close()
+Bumble_Honey_bees_f.close()
+Outgroup_f.close()
+Ortho_tab.close()
+     
