62afc13bf71b03d81d048ccf88bee04d1a59da06
JoClaivaz <joaquimclaivaz@gmail.com>
2017-04-25 17:23:32 +0200

    add space to make the scripts more readable

--- scripts/proteinseqextract.py
+++ scripts/proteinseqextract.py
@@ -5,41 +5,41 @@ from Bio.SeqRecord import SeqRecord
 import os
 
 def extractprotseq(bee_group, bee_folder, all_proteins_faa):
-	bee_group_f=open(bee_group, 'r')
-	all_prot_f=open(all_proteins_faa, 'r')
+	bee_group_f = open(bee_group, 'r')
+	all_prot_f = open(all_proteins_faa, 'r')
 
 	#os.chdir(bee_folder)
 
-	genome_protseq={} #dico of each ref_genome|protein_ID and its amino acid sequence
+	genome_protseq = {} #dico of each ref_genome|protein_ID and its amino acid sequence
 	for seq_record in SeqIO.parse(all_prot_f, 'fasta'):
-		genome_protseq[seq_record.id]=seq_record.seq
+		genome_protseq[seq_record.id] = seq_record.seq
 
 
 	for line in bee_group_f:
-		lline=line.split()
+		lline = line.split()
 
-		sequences=[] #list of ref_genome|protein_ID and its amino acid sequence of a Gene_family
-		Gene_family=''		
-		Gene_family=lline[0]
-		prot_seq_fn=Gene_family + '.fasta' #output names
+		sequences = [] #list of ref_genome|protein_ID and its amino acid sequence of a Gene_family
+		Gene_family = ''		
+		Gene_family = lline[0]
+		prot_seq_fn = Gene_family + '.fasta' #output names
 		
 
-		lline_wo_0 =lline[1::] #line without first element = Gene_family_*
+		lline_wo_0 = lline[1::] #line without first element = Gene_family_*
 
 		for obj in lline_wo_0:
 			if obj in genome_protseq.keys():
-				record=SeqRecord(genome_protseq[obj], id=obj)
+				record = SeqRecord(genome_protseq[obj], id=obj)
 				sequences.append(record)
 			else:
 				print('obj not found in all_prot_f', obj)
 		SeqIO.write(sequences, bee_folder+prot_seq_fn, 'fasta')
-		print(bee_group,'-', Gene_family, 'Done.')
+		print(bee_group, '-', Gene_family, 'Done.')
 
 
 
 
-extractprotseq(bee_group='data/sort_group/Bumble_bees.txt', bee_folder='data/Bumble_bees_proteins/', all_proteins_faa='/scratch/beegfs/monthly/mls_2016/blast/all_proteins.fasta')
+extractprotseq(bee_group = 'data/sort_group/Bumble_bees.txt', bee_folder = 'data/Bumble_bees_proteins/', all_proteins_faa = '/scratch/beegfs/monthly/mls_2016/blast/all_proteins.fasta')
 
-extractprotseq(bee_group='data/sort_group/Honey_bees.txt', bee_folder='data/Honey_bees_proteins/', all_proteins_faa='/scratch/beegfs/monthly/mls_2016/blast/all_proteins.fasta')
+extractprotseq(bee_group = 'data/sort_group/Honey_bees.txt', bee_folder = 'data/Honey_bees_proteins/', all_proteins_faa = '/scratch/beegfs/monthly/mls_2016/blast/all_proteins.fasta')
 
-extractprotseq(bee_group='data/sort_group/Bumble_Honey_bees.txt', bee_folder='data/Bumble_Honey_bees_proteins/', all_proteins_faa='/scratch/beegfs/monthly/mls_2016/blast/all_proteins.fasta')
+extractprotseq(bee_group = 'data/sort_group/Bumble_Honey_bees.txt', bee_folder = 'data/Bumble_Honey_bees_proteins/', all_proteins_faa = '/scratch/beegfs/monthly/mls_2016/blast/all_proteins.fasta')
--- scripts/subgroup_sort.py
+++ scripts/subgroup_sort.py
@@ -10,29 +10,29 @@ Subgroup considered:Bombus sp. & Apis sp. & Apis/Bombus sp.
 
 #####create different subgroup
 #'JF72':Lactobacillus apis ,'JF73':Lactobacillus helsingborgensis,'JF74':Lactobacillus melliventris,'JF75':Lactobacillus kimbladii,'JF76':Lactobacillus kullabergensis,'JG29':Lactobacillus mellis,'JG30':Lactobacillus mellifer,'L183':L. helsingborgensis,'L184':L melliventris, 'L185': L apis, 'L186': L Kullabergensis, 'LA14': Lactobacillus acidophilus (mammalian commensale/yoghurt), 'LA2':Lactobacillus amylovorus (pig) , 'LDB': L. delbrueckii (yoghurt), 'LGAS':Lactobacillus gasseri (Prebiotic/probiotic), 'LHV': Lactobacillus helveticus (cheese process),'LJP': Lactobacillus johnsonii (probiot/prebiot), 'WANG': L kefiranofaciens (pro/prebiot/kefir)
-B_pascuorum=['F225','F230','F233','F234','F236','F237']
-B_bohemicus=['F228', 'F245','F246','F247']
-Bumble=B_pascuorum+B_bohemicus
-A_mellifera=['JF72','JF73','JG30','JF74','JF75','JF76','F259','F260','F261','F262','F263','L183','L184','L185','L186']
-Honey=A_mellifera
-Outgroup=['JG29','LA14','LA2','LDB','LGAS','LHV','LJP','WANG']
+B_pascuorum = ['F225', 'F230', 'F233', 'F234', 'F236', 'F237']
+B_bohemicus = ['F228', 'F245', 'F246', 'F247']
+Bumble = B_pascuorum + B_bohemicus
+A_mellifera = ['JF72', 'JF73', 'JG30', 'JF74', 'JF75', 'JF76', 'F259', 'F260', 'F261', 'F262', 'F263', 'L183', 'L184', 'L185', 'L186']
+Honey = A_mellifera
+Outgroup = ['JG29', 'LA14', 'LA2', 'LDB', 'LGAS', 'LHV', 'LJP', 'WANG']
 #####loop writing the correct set/subgroup
-Ortho_tab=open("data/GeneFamilies.txt",'r')
-Bumble_bees_f=open("data/sort_group/Bumble_bees.txt",'w') #Bumble set
-Honey_bees_f=open("data/sort_group/Honey_bees.txt","w") #Apis set
-Bumble_Honey_bees_f=open("data/sort_group/Bumble_Honey_bees.txt","w") #Bumble and Apis set
-Outgroup_f=open("data/sort_group/Outgroup.txt","w")
+Ortho_tab = open("data/GeneFamilies.txt", 'r')
+Bumble_bees_f = open("data/sort_group/Bumble_bees.txt", 'w') #Bumble set
+Honey_bees_f = open("data/sort_group/Honey_bees.txt", "w") #Apis set
+Bumble_Honey_bees_f = open("data/sort_group/Bumble_Honey_bees.txt", "w") #Bumble and Apis set
+Outgroup_f = open("data/sort_group/Outgroup.txt", "w")
 
-def are_all_in_line(group_bee,line,threshold):
-    threshold_limit=1
-    one_strain_present=False
+def are_all_in_line(group_bee, line, threshold):
+    threshold_limit = 1
+    one_strain_present = False
     for strain in group_bee:
         if strain not in line:
-            threshold_limit-=1/len(group_bee)
+            threshold_limit -= 1/len(group_bee)
             if threshold_limit < threshold and one_strain_present:
                 return 'Partial'
         else:
-            one_strain_present=True
+            one_strain_present = True
     if threshold_limit > threshold:
         return True
     elif one_strain_present:
@@ -40,21 +40,21 @@ def are_all_in_line(group_bee,line,threshold):
     else:
         return False
 
-threshold=0.8
+threshold = 0.8
 ###
 for line in Ortho_tab:
-    Bumble_tmp=are_all_in_line(Bumble,line,threshold)
-    Honey_tmp=are_all_in_line(Honey,line,threshold)
-    Outgroup_tmp=are_all_in_line(Outgroup,line,threshold)
+    Bumble_tmp = are_all_in_line(Bumble,line,threshold)
+    Honey_tmp = are_all_in_line(Honey,line,threshold)
+    Outgroup_tmp = are_all_in_line(Outgroup,line,threshold)
     if not Outgroup_tmp:
-        if Bumble_tmp==True:
-            if Honey_tmp==True:
+        if Bumble_tmp == True:
+            if Honey_tmp == True:
                 Bumble_Honey_bees_f.write(line)
             elif not Honey_tmp:
                 Bumble_bees_f.write(line)
             else:
                 Outgroup_f.write(line)
-        elif not Bumble_tmp and Honey_tmp==True:
+        elif not Bumble_tmp and Honey_tmp == True:
             Honey_bees_f.write(line)
         else:
             Outgroup_f.write(line)
