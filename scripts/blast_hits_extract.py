f3b494e81a8d46fee12c002c7a41a8b11957b8d5
JoClaivaz <joaquimclaivaz@gmail.com>
2017-05-22 12:10:56 +0200

    solved indentation problem

--- scripts/blast_hits_extract.py
+++ scripts/blast_hits_extract.py
@@ -17,36 +17,36 @@ threshold_bitscore = sys.argv[6]
 
 def parsed_blast_hits(input_file, bee_folder, threshold_alignment_length, threshold_ID, threshold_eval, threshold_bitscore):
 
-	if input_file != 'list_files.txt': 
-    bee_file = open('data/blast/' + bee_folder + '/' + input_file, 'r')
-    bee_file_out = open('data/parsed_blast/' + bee_folder +'/' + input_file.split('.out')[0] + '_parsed.out','w')
-    bee_file_out.write('# Query_ID\tSubject_titles\t%_Identity\tAlignment_length\tevalue\tbit_score\n')
-    bee_file_list = []
-    threshold_ID_tmp = float(threshold_ID)
-    for line in bee_file:
-        if '#' not in line:
-            tmp_split_line = line.split('\t')
-            if (float(tmp_split_line[7]) - float(tmp_split_line[6])) / (float(tmp_split_line[9]) - float(tmp_split_line[8])) >= float(threshold_alignment_length):
-                if float(tmp_split_line[2]) >= float(threshold_ID_tmp):
-                    if float(tmp_split_line[10]) <= float(threshold_eval):
-                        if float(tmp_split_line[11]) >= float(threshold_bitscore):
-                            bee_file_list.append('%s\t%s\t%s\t%s\t%s\t%s\n'%(tmp_split_line[0], tmp_split_line[13].replace('.\n',''), tmp_split_line[2], tmp_split_line[3], tmp_split_line[10], tmp_split_line[11]))
-    if len(bee_file_list) > 25:
-        for blast_hit in bee_file_list:
-            bee_file_out.write(blast_hit)
-    elif float(threshold_ID_tmp) > 20:
-        parsed_blast_hits(input_file = bee_file, bee_folder = bee_folder,
-                          threshold_alignment_length = threshold_alignment_length,
-                          threshold_ID = 0.8 * float(threshold_ID_tmp), 
-                          threshold_eval = threshold_eval,
-                          threshold_bitscore = threshold_bitscore)
-    else:
-        for blast_hit in bee_file_list:
-            bee_file_out.write(blast_hit)
-
-        
-    bee_file_out.close()
-    bee_file.close()
-        
+	 if input_file != 'list_files.txt': 
+        bee_file = open('data/blast/' + bee_folder + '/' + input_file, 'r')
+        bee_file_out = open('data/parsed_blast/' + bee_folder +'/' + input_file.split('.out')[0] + '_parsed.out','w')
+        bee_file_out.write('# Query_ID\tSubject_titles\t%_Identity\tAlignment_length\tevalue\tbit_score\n')
+        bee_file_list = []
+        threshold_ID_tmp = float(threshold_ID)
+        for line in bee_file:
+            if '#' not in line:
+                tmp_split_line = line.split('\t')
+                if (float(tmp_split_line[7]) - float(tmp_split_line[6])) / (float(tmp_split_line[9]) - float(tmp_split_line[8])) >= float(threshold_alignment_length):
+                    if float(tmp_split_line[2]) >= float(threshold_ID_tmp):
+                        if float(tmp_split_line[10]) <= float(threshold_eval):
+                            if float(tmp_split_line[11]) >= float(threshold_bitscore):
+                                bee_file_list.append('%s\t%s\t%s\t%s\t%s\t%s\n'%(tmp_split_line[0], tmp_split_line[13].replace('.\n',''), tmp_split_line[2], tmp_split_line[3], tmp_split_line[10], tmp_split_line[11]))
+        if len(bee_file_list) > 25:
+            for blast_hit in bee_file_list:
+                bee_file_out.write(blast_hit)
+        elif float(threshold_ID_tmp) > 20:
+            parsed_blast_hits(input_file = bee_file, bee_folder = bee_folder,
+                              threshold_alignment_length = threshold_alignment_length,
+                              threshold_ID = 0.8 * float(threshold_ID_tmp), 
+                              threshold_eval = threshold_eval,
+                              threshold_bitscore = threshold_bitscore)
+        else:
+            for blast_hit in bee_file_list:
+                bee_file_out.write(blast_hit)
+    
+            
+        bee_file_out.close()
+        bee_file.close()
+            
 parsed_blast_hits(input_file, bee_folder, threshold_alignment_length, threshold_ID, threshold_eval, threshold_bitscore)        
         
