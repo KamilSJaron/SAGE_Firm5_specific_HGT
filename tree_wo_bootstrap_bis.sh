b1ef4d036537ce2867f1d77e36e1fdc3f3bc9535
Virginie Ricci <vricci@frt.vital-it.ch>
2017-06-01 13:18:55 +0000

    tree_wo_bootstrap_bis.sh

--- tree_wo_bootstrap_bis.sh
+++ tree_wo_bootstrap_bis.sh
@@ -26,8 +26,8 @@ FigTree tool (available at http://tree.bio.ed.ac.uk/software/figtree/)
 #BSUB -o tree_wo_bootstrap.out
 #BSUB -e tree_wo_bootstrap.err
 #BSUB -J tree_wo_bootstrap
-#BSUB -n 12
-#BSUB -R "span[ptile=12]"
+#BSUB -n 18
+#BSUB -R "span[ptile=18]"
 #BSUB -M 10000000
 #BSUB –R "rusage[mem=10000]"
 #BSUB -u virginie.ricci@unil.ch
@@ -38,19 +38,28 @@ module add Phylogeny/raxml/8.2.9;
 
 cd /scratch/beegfs/monthly/mls_2016/claivaz_ricci/SAGE_Firm5_specific_HGT/data/alignment_potential_HGT/
 
+mkdir -p RAxML_results
 
-raxmlHPC -m PROTGAMMAGTR -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_1058_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1058_parsed.out
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_1058_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1058_parsed.out
 
-raxmlHPC -m PROTGAMMAGTR -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_991_parsed.multifasta -n ML_wo_bootstrap_Gene_family_991_parsed.out
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_991_parsed.multifasta -n ML_wo_bootstrap_Gene_family_991_parsed.out
 
-raxmlHPC -m PROTGAMMAGTR -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_1099_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1099_parsed.out
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_1099_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1099_parsed.out
 
-raxmlHPC -m PROTGAMMAGTR -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_1048_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1048_parsed.out
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_Honey_bees_proteins/Gene_family_1048_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1048_parsed.out
 
-raxmlHPC -m PROTGAMMAGTR -p 12345 -s Bumble_bees_proteins/Gene_family_1674_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1674_parsed.out
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_bees_proteins/Gene_family_1674_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1674_parsed.out
+
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_bees_proteins/Gene_family_1675_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1675_parsed.out
+
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_bees_proteins/Gene_family_1678_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1678_parsed.out
+
+raxmlHPC -m PROTGAMMAAUTO -p 12345 -s Bumble_bees_proteins/Gene_family_1757_parsed.multifasta -n ML_wo_bootstrap_Gene_family_1757_parsed.out
 
 
-# -m : substitutionModel - amino acid substitution model: GTR
+mv RAxML_* RAxML_results/
+
+# -m : substitutionModel - amino acid substitution model: AUTO will test prot substitution models with and without empirical base frequencies
 # -p : Specify a random number seed for the parsimony inferences. This allows you to reproduce your results and will help me debug the program.
 # -s : input
 # -n : output
